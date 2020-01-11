f8f0a0f970fdae7a74357fc6d46668589741f1d7
Tatum Daily <tgd9019@outlook.com>
2020-01-11 14:07:25 -0600

    Final Draft

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,165 +1,175 @@
+$(document).ready( function() {
+
 // Global Variables //
 // ========================================================== //
 
     // User Stats //
     // ======================== //  
 
-        var wins = 0;
-        var losses = 0;
+    var wins = 0;
+    var losses = 0;
 
-    // User Score //
-    // ======================== // 
+// User Score //
+// ======================== // 
 
-        var userScore = 0;
+    var userScore = 0;
 
-    // Computer Number (Number user is to guess) //
-    // ======================== // 
+// Computer Number (Number user is to guess) //
+// ======================== // 
 
-        var computerNumber = compNum();
+    var computerNumber = compNum();
 
-    // Crystal Value //
-    // ======================== // 
+// Crystal Value //
+// ======================== // 
 
-        var rubyValue = crysVal();
-        var diamondValue = crysVal();
-        var sapphireValue = crysVal();
-        var emeraldValue = crysVal();
-        
+    var rubyValue = crysVal();
+    var diamondValue = crysVal();
+    var sapphireValue = crysVal();
+    var emeraldValue = crysVal();
+    
 // Functions //
 // ========================================================== //
 
-    // Initial Page Load //
-    // ======================== // 
+// Initial Page Load //
+// ======================== // 
 
-        function pageLoad() {
-            $("#ruby").val(rubyValue)
-            $("#diamond").val(diamondValue)
-            $("#sapphire").val(sapphireValue)
-            $("#emerald").val(emeraldValue)  
-        }
+    function pageLoad() {
+        rubyValue = crysVal();
+        diamondValue = crysVal();
+        sapphireValue = crysVal();
+        emeraldValue = crysVal();
 
-    // Generate Computer Number //
-    // ======================== // 
+        $("#ruby").val(rubyValue)
+        $("#diamond").val(diamondValue)
+        $("#sapphire").val(sapphireValue)
+        $("#emerald").val(emeraldValue)  
+    }
 
-        function compNum(min, max) {
-            var min = 19;
-            var max = 120;
+// Generate Computer Number //
+// ======================== // 
 
-            return Math.floor(Math.random() * (max - min + 1)) + min;
-        }
+    function compNum(min, max) {
+        var min = 19;
+        var max = 120;
 
-    // Generate Crystal Value //
-    // ======================== // 
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 
-        function crysVal(min, max) {
-            var min = 1;
-            var max = 12;
+// Generate Crystal Value //
+// ======================== // 
 
-            return Math.floor(Math.random() * (max - min + 1)) + min;
-        }      
+    function crysVal(min, max) {
+        var min = 1;
+        var max = 12;
 
-    // When clicked, add Crystal Value to User Score //
-    // ======================== // 
-        
-        $("#ruby").on("click", function rubyClick() {
-            userScore = userScore + rubyValue;
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }      
 
-            document.getElementById("userScore").innerHTML = "Your number is currently at: " + "    " + userScore;
-            
-            if (userScore === computerNumber) {
-                wins++
-                document.getElementById("userStats").innerHTML = "Wins: " + wins + "    " + "Losses: " + losses;
-                reset();
-            }
-
-            if (userScore > computerNumber) {
-                losses++
-                document.getElementById("userStats").innerHTML = "Wins: " + wins + "    " + "Losses: " + losses;
-                reset();
-            }
-            
-        });
-        
-        $("#diamond").on("click", function diamondClick() {
-            userScore = userScore + diamondValue;
-
-            document.getElementById("userScore").innerHTML = "Your number is currently at: " + "    " + userScore;
-
-            if (userScore === computerNumber) {
-                wins++
-                document.getElementById("userStats").innerHTML = "Wins: " + wins + "    " + "Losses: " + losses;
-                reset();
-            }
-
-            if (userScore > computerNumber) {
-                losses++
-                document.getElementById("userStats").innerHTML = "Wins: " + wins + "    " + "Losses: " + losses;
-                reset();
-            }
-        });
-
-        $("#sapphire").on("click", function sapphireClick() {
-            userScore = userScore + sapphireValue;
-
-            document.getElementById("userScore").innerHTML = "Your number is currently at: " + "    " + userScore;
-
-            if (userScore === computerNumber) {
-                wins++
-                document.getElementById("userStats").innerHTML = "Wins: " + wins + "    " + "Losses: " + losses;
-                reset();
-            }
-
-            if (userScore > computerNumber) {
-                losses++
-                document.getElementById("userStats").innerHTML = "Wins: " + wins + "    " + "Losses: " + losses;
-                reset();
-            }
-        });
-
-        $("#emerald").on("click", function emeraldClick() {
-            userScore = userScore + emeraldValue;
-
-            document.getElementById("userScore").innerHTML = "Your number is currently at: " + "    " + userScore;
-
-            if (userScore === computerNumber) {
-                wins++
-                document.getElementById("userStats").innerHTML = "Wins: " + wins + "    " + "Losses: " + losses;
-                reset();
-            }
-
-            if (userScore > computerNumber) {
-                losses++
-                document.getElementById("userStats").innerHTML = "Wins: " + wins + "    " + "Losses: " + losses;
-                reset();
-            }
-        });
+// When clicked, add Crystal Value to User Score //
+// ======================== // 
     
-    // Reset //
-    // ======================== // 
+    $("#ruby").on("click", function rubyClick() {
+        userScore = userScore + rubyValue;
 
-        function reset() {
-            computerNumber = compNum();
-            userScore = 0;
-
-            document.getElementById("gameNumber").innerHTML = "Number to match: " + "    " + computerNumber;
-            document.getElementById("userScore").innerHTML = "Your number is currently at: " + "    " + userScore;            
+        $("#userScore").html("Your number is currently at: " + "    " + userScore); 
+        
+        if (userScore === computerNumber) {
+            wins++
+            $("#userStats").html("Wins: " + wins + "    " + "Losses: " + losses);
+            reset();
         }
+
+        if (userScore > computerNumber) {
+            losses++
+            $("#userStats").html("Wins: " + wins + "    " + "Losses: " + losses);
+            reset();
+        }
+        
+    });
+    
+    $("#diamond").on("click", function diamondClick() {
+        userScore = userScore + diamondValue;
+
+        $("#userScore").html("Your number is currently at: " + "    " + userScore);
+
+        if (userScore === computerNumber) {
+            wins++
+            $("#userStats").html("Wins: " + wins + "    " + "Losses: " + losses);
+            reset();
+        }
+
+        if (userScore > computerNumber) {
+            losses++
+            $("#userStats").html("Wins: " + wins + "    " + "Losses: " + losses);
+            reset();
+        }
+    });
+
+    $("#sapphire").on("click", function sapphireClick() {
+        userScore = userScore + sapphireValue;
+
+        $("#userScore").html("Your number is currently at: " + "    " + userScore);
+
+        if (userScore === computerNumber) {
+            wins++
+            $("#userStats").html("Wins: " + wins + "    " + "Losses: " + losses);
+            reset();
+        }
+
+        if (userScore > computerNumber) {
+            losses++
+            $("#userStats").html("Wins: " + wins + "    " + "Losses: " + losses);
+            reset();
+        }
+    });
+
+    $("#emerald").on("click", function emeraldClick() {
+        userScore = userScore + emeraldValue;
+
+        $("#userScore").html("Your number is currently at: " + "    " + userScore);
+
+        if (userScore === computerNumber) {
+            wins++
+            $("#userStats").html("Wins: " + wins + "    " + "Losses: " + losses);
+            reset();
+        }
+
+        if (userScore > computerNumber) {
+            losses++
+            $("#userStats").html("Wins: " + wins + "    " + "Losses: " + losses);
+            reset();
+        }
+    });
+
+// Reset //
+// ======================== // 
+
+    function reset() {
+        
+
+        pageLoad()
+        computerNumber = compNum();
+        userScore = 0;
+
+        $("#gameNumber").html("Number to match: " + "    " + computerNumber);
+        $("#userScore").html("Your number is currently at: " + "    " + userScore);            
+    }
 
 // Main Process //
 // ========================================================== //
 
-    // Initial Page Load //
-    // ======================== // 
-        
-        document.getElementById("gameNumber").innerHTML = "Number to match: " + "    " + computerNumber;
-        document.getElementById("userStats").innerHTML = "Wins: " + wins + "    " + "Losses: " + losses;
-        document.getElementById("userScore").innerHTML = "Your number is currently at: " + "    " + userScore;
-        
-        $(document).ready(pageLoad);
+// Initial Page Load //
+// ======================== // 
+    
+    $("#gameNumber").html("Number to match: " + "    " + computerNumber);
+    $("#userStats").html("Wins: " + wins + "    " + "Losses: " + losses);
+    $("#userScore").html("Your number is currently at: " + "    " + userScore);
+    
+});
+
+
 
     
-
-        
-        
-       
-       
+    
+   
+   
